add51826ab32da27d86c08c01a7b5aecbeddee3d
Emrullah AKAN <akanemrullah@gmail.com>
2024-02-20 04:10:32 +0300

    Guncel

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,5 @@
 public class Main {
     public static void main(String[] args) {
 
-
-        // Temel operatorler
-
-        // Atama Operatoru
-
-        // Temel Atama Operatoru ( = )
-
-        int a = 5, b = 8; // 5 ve 8 degerlerini a ve b degiskenlerine atadik.
-        // a + b sonucunu c degiskenine atadik.
-
-        // Operatorun saginda ki degeri solunda ki deger ile toplayip tekrar solunda ki degere atayan operatordur. ( += )
-
-        a += b;
-
-        // Operatorun saginda ki degeri solunda ki degerden cikartip tekrar solunda ki degere atayan operatordur. ( -= )
-
-        a -= b;
-
-        // Operatorun saginda ki degeri solunda ki deger ile carpip tekrar solunda ki degere atayan operatordur. ( *= )
-
-        a *= b;
-
-        // Operatorun saginda ki degeri solunda ki degere ile bolup tekrar solunda ki degere atayan operatordur. ( /= )
-
-        a /= b;
-
-        // Operatorun saginda ki degerin solunda ki ne gore modunu alip tekrar solunda ki degere atayan operatordur. ( %= )
-
-        // Aritmetik Operatorler
-        // Java da aritmetik operatorler adindan da anlasilacagi uzre matematiksel islemleri programlama dilinde uygulamamizi saglar.
-
-
     }
 }
