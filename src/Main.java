5751f7bcdc68d2698cec5c0d45233734d2777d96
Emrullah AKAN <akanemrullah@gmail.com>
2024-02-20 07:46:40 +0300

    Guncel

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
 
         Scanner input = new Scanner(System.in);
 
+        // Intiger islemleri
+
         System.out.print("Merhabalar. Toplamak istedigin ilk degeri gir: ");
         int a = input.nextInt();
 
@@ -12,6 +14,18 @@ public class Main {
         int b = input.nextInt();
 
         System.out.println("Sonuc = " + (a + b));
+
+        // Double islemleri
+
+        System.out.print("Sayi gir: ");
+        double c = input.nextDouble();
+        System.out.println("Girdiginiz sayi : " + c);
+
+        // String islemleri
+
+        System.out.println("Merhabalar. Isminizi giriniz lutfen : ");
+        String d = input.nextLine();
+        System.out.println("Hosgeldin " + d + " Iyi vakitler...");
 
     }
 }
