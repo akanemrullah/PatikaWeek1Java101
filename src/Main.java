5a774d19d9d10aaf121d78bbe9eff342cea3a532
Emrullah AKAN <akanemrullah@gmail.com>
2024-02-20 04:35:47 +0300

    Guncel

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Merhaba Dunyaa!");
-        System.out.println("Sanada Merhaba!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Merhabalar. Toplamak istedigin ilk degeri gir: ");
+        int a = input.nextInt();
+
+        System.out.print("Toplamak istedigin ikinci degeri gir:");
+        int b = input.nextInt();
+
+        System.out.println("Sonuc = " + (a + b));
 
     }
 }
