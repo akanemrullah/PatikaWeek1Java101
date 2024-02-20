f4049b0cc4ea647c823a84cc2c3ba9368098c482
Emrullah AKAN <akanemrullah@gmail.com>
2024-02-20 04:03:36 +0300

    Guncel

--- src/Main.java
+++ src/Main.java
@@ -10,37 +10,31 @@ public class Main {
 
         int a = 5, b = 8; // 5 ve 8 degerlerini a ve b degiskenlerine atadik.
         int c = a + b; // a + b sonucunu c degiskenine atadik.
-        System.out.println(a + " " + c);
 
         // Operatorun saginda ki degeri solunda ki deger ile toplayip tekrar solunda ki degere atayan operatordur. ( += )
 
         a += b;
         c += a;
-        System.out.println(a + " " + c);
 
         // Operatorun saginda ki degeri solunda ki degerden cikartip tekrar solunda ki degere atayan operatordur. ( -= )
 
         a -= b;
         c -= a;
-        System.out.println(a + " " + c);
 
         // Operatorun saginda ki degeri solunda ki deger ile carpip tekrar solunda ki degere atayan operatordur. ( *= )
 
         a *= b;
         c *= a;
-        System.out.println(a + " " + c);
 
         // Operatorun saginda ki degeri solunda ki degere ile bolup tekrar solunda ki degere atayan operatordur. ( /= )
 
         a /= b;
         c /= a;
-        System.out.println(a + " " + c);
 
         // Operatorun saginda ki degerin solunda ki ne gore modunu alip tekrar solunda ki degere atayan operatordur. ( %= )
 
         a %= b;
         c %= a;
-        System.out.println(a + " " + c);
 
         // Aritmetik Operatorler
         // Java da aritmetik operatorler adindan da anlasilacagi uzre matematiksel islemleri programlama dilinde uygulamamizi saglar.
@@ -50,7 +44,6 @@ public class Main {
         c = a * b; // Carpma
         c = a / b; // Bolme
 
-        System.out.println("Aritmetik Operatorler : " + c);
 
     }
 }
